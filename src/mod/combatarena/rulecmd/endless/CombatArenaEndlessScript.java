b75d6d9602cdc5848e6776ba1033d741e77d15bb
Darkborderman <reastw1234@gmail.com>
2022-08-10 02:49:09 +0800

    refactor: Update

--- src/mod/combatarena/rulecmd/endless/CombatArenaEndlessScript.java
+++ src/mod/combatarena/rulecmd/endless/CombatArenaEndlessScript.java
@@ -5,12 +5,9 @@ import java.util.Map;
 
 import org.lwjgl.input.Keyboard;
 
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.OptionPanelAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
-import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.util.Misc;
 
@@ -23,11 +20,11 @@ public class CombatArenaEndlessScript extends BaseCommandPlugin {
         opts.clearOptions();
 
         opts.addOption("Fight", "CombatArenaTokenExchangeBlueprintWeaponOption");
-        opts.addOption("Change faction", "CombatArenaTokenExchangeBlueprintFighterOption");
-        opts.addOption("Redeem hullmod blueprint(4 token)", "CombatArenaTokenExchangeBlueprintHullmodOption");
-        opts.addOption("Redeem hull blueprint(8 token)", "CombatArenaTokenExchangeBlueprintHullOption");
-        opts.addOption("Back", "CombatArenaTokenExchangeOption");
-        opts.setShortcut("CombatArenaTokenExchangeOption", Keyboard.KEY_ESCAPE, false, false, false, false);
+        opts.addOption("Change opponent faction", "CombatArenaTokenExchangeBlueprintFighterOption");
+        //opts.addOption("Change opponent size", "CombatArenaTokenExchangeBlueprintHullmodOption");
+        //opts.addOption("Change opponent combat strength", "CombatArenaTokenExchangeBlueprintHullOption");
+        opts.addOption("Back", "CombatArenaMainEntryOption");
+        opts.setShortcut("CombatArenaMainEntryOption", Keyboard.KEY_ESCAPE, false, false, false, false);
         return true;
     }
 }
