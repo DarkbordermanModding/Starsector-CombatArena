7a00c38527320ccb09954705244992e0b57f4643
Darkborderman <reastw1234@gmail.com>
2022-08-12 23:36:49 +0800

    refactor: Update

--- src/mod/combatarena/rulecmd/tokenexchange/CombatArenaTokenExchangeMainEntry.java
+++ src/mod/combatarena/rulecmd/tokenexchange/CombatArenaTokenExchangeMainEntry.java
@@ -15,11 +15,11 @@ public class CombatArenaTokenExchangeMainEntry extends BaseCommandPlugin {
 
     @Override
     public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
-        dialog.getTextPanel().addParagraph("Welcome to the Combat arena, a place for you to spill the blood and test your maximum.");
         OptionPanelAPI opts = dialog.getOptionPanel();
         opts.clearOptions();
         opts.addOption("Redeem token for blueprints", "CombatArenaTokenExchangeBlueprintOption");
         opts.addOption("Redeem token for cores", "CombatArenaTokenExchangeCoreOption");
+        opts.addOption("Redeem token for credits", "CombatArenaTokenExchangeCreditOption");
         opts.addOption("Leave", "CombatArenaMainEntryOption");
         opts.setShortcut("CombatArenaMainEntryOption", Keyboard.KEY_ESCAPE, false, false, false, false);
         return true;
