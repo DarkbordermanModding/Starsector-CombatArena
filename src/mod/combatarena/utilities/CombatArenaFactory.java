22d12cf3a4ef148ce9e4bfa7070f8e1f478b55fa
Darkborderman <reastw1234@gmail.com>
2022-08-15 00:24:34 +0800

    refactor: Cleanup code

--- src/mod/combatarena/utilities/CombatArenaFactory.java
+++ src/mod/combatarena/utilities/CombatArenaFactory.java
@@ -5,7 +5,6 @@ import java.util.Random;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignFleetAPI;
-import com.fs.starfarer.api.campaign.FactionDoctrineAPI;
 import com.fs.starfarer.api.campaign.FleetAssignment;
 import com.fs.starfarer.api.campaign.FactionAPI.ShipPickMode;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
@@ -14,9 +13,7 @@ import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import static com.fs.starfarer.api.impl.campaign.fleets.FleetFactoryV3.*;
 import com.fs.starfarer.api.impl.campaign.fleets.FleetParamsV3;
 import com.fs.starfarer.api.impl.campaign.ids.FleetTypes;
-import com.fs.starfarer.api.impl.campaign.ids.HullMods;
 import com.fs.starfarer.api.impl.campaign.ids.MemFlags;
-import com.fs.starfarer.api.impl.campaign.ids.Stats;
 import com.fs.starfarer.api.util.Misc;
 import org.apache.log4j.Logger;
 
@@ -24,127 +21,6 @@ public class CombatArenaFactory {
 
     public static final Logger LOG = Global.getLogger(CombatArenaFactory.class);
 
-    public static CampaignFleetAPI createFleet(FleetParamsV3 params) {
-        LOG.info("|||   Creating Fleet Begin   |||");
-        try {
-            params.ignoreMarketFleetSizeMult = true;
-
-            MarketAPI market = pickMarket(params);
-            if (market == null) {
-                market = Global.getFactory().createMarket("fake", "fake", 5);
-                market.getStability().modifyFlat("fake", 10000);
-                market.setFactionId(params.factionId);
-                market.getStats().getDynamic().getMod(Stats.FLEET_QUALITY_MOD).modifyFlat("fake", BASE_QUALITY_WHEN_NO_MARKET);
-                market.getStats().getDynamic().getMod(Stats.COMBAT_FLEET_SIZE_MULT).modifyFlat("fake", 1f);
-            }
-            boolean sourceWasNull = params.source == null;
-            params.source = market;
-            if (sourceWasNull && params.qualityOverride == null) { // we picked a nearby market based on location
-                params.updateQualityAndProducerFromSourceMarket();
-            }
-
-            String factionId = params.factionId;
-
-            if(factionId.equals("combat_arena")) params.mode = ShipPickMode.ALL;
-            else params.mode = ShipPickMode.PRIORITY_THEN_ALL;
-
-            CampaignFleetAPI fleet = createEmptyFleet(factionId, params.fleetType, market);
-            fleet.getFleetData().setOnlySyncMemberLists(true);
-
-            FactionDoctrineAPI doctrine = fleet.getFaction().getDoctrine();
-
-            Random random = new Random();
-
-            float combatPts = params.combatPts;
-
-            float dW = (float) doctrine.getWarships() + random.nextInt(3) - 2;
-            float dC = (float) doctrine.getCarriers() + random.nextInt(3) - 2;
-            float dP = (float) doctrine.getPhaseShips() + random.nextInt(3) - 2;
-
-            float r1 = random.nextFloat();
-            float r2 = random.nextFloat();
-            float min = Math.min(r1, r2);
-            float max = Math.max(r1, r2);
-
-            float mag = 1f;
-            float v1 = min;
-            float v2 = max - min;
-            float v3 = 1f - max;
-
-            v1 *= mag;
-            v2 *= mag;
-            v3 *= mag;
-
-            v1 -= mag / 3f;
-            v2 -= mag / 3f;
-            v3 -= mag / 3f;
-
-            dW += v1;
-            dC += v2;
-            dP += v3;
-
-            params.banPhaseShipsEtc = false;
-
-            if (dW < 0) {
-                dW = 0;
-            }
-            if (dC < 0) {
-                dC = 0;
-            }
-            if (dP < 0) {
-                dP = 0;
-            }
-
-            float extra = 7 - (dC + dP + dW);
-            if (extra < 0) {
-                extra = 0f;
-            }
-            if (doctrine.getWarships() > doctrine.getCarriers() && doctrine.getWarships() > doctrine.getPhaseShips()) {
-                dW += extra;
-            } else if (doctrine.getCarriers() > doctrine.getWarships() && doctrine.getCarriers() > doctrine.getPhaseShips()) {
-                dC += extra;
-            } else if (doctrine.getPhaseShips() > doctrine.getWarships() && doctrine.getPhaseShips() > doctrine.getCarriers()) {
-                dP += extra;
-            }
-
-            float doctrineTotal = dW + dC + dP;
-
-            //System.out.println("DW: " + dW + ", DC: " + dC + " DP: " + dP);
-            combatPts = (int) combatPts;
-            int warships = (int) (combatPts * dW / doctrineTotal);
-            int carriers = (int) (combatPts * dC / doctrineTotal);
-            int phase = (int) (combatPts * dP / doctrineTotal);
-
-            warships += (combatPts - warships - carriers - phase);
-            params.combatPts = 0f;
-            addCombatFleetPoints(fleet, random, warships, carriers, phase, params);
-            fleet.getFleetData().sort();
-            fleet.forceSync();
-
-            fleet.getFleetData().setOnlySyncMemberLists(false);
-            fleet.getFleetData().sort();
-
-            // Do some custom fleet advanced options
-            List<FleetMemberAPI> members = fleet.getFleetData().getMembersListCopy();
-            for (FleetMemberAPI member : members) {
-                // member.setCaptain(
-                //     OfficerManagerEvent.createOfficer(
-                //         Global.getSector().getFaction(factionId), 1, true
-                //     )
-                // );
-                // member.getVariant().addMod(HullMods.REINFORCEDHULL);
-                member.getRepairTracker().setCR(member.getRepairTracker().getMaxCR());
-            }
-            fleet.setNoFactionInName(true);
-            fleet.setFaction("combat_arena", true);
-            fleet.setName("Opponent fleet");
-            fleet.getAI().addAssignment(FleetAssignment.INTERCEPT, Global.getSector().getPlayerFleet(), 1000000f, null);
-            fleet.getMemoryWithoutUpdate().set(MemFlags.MEMORY_KEY_MAKE_AGGRESSIVE, true);
-            fleet.getMemoryWithoutUpdate().set("$dialog", "The opponent glares at you briefly before shutting down the comm link.");
-            Misc.makeImportant(fleet, "combat_arena", 120);
-            return fleet;
-        } finally {}
-    }
     public static CampaignFleetAPI createFleetV2(CombatArenaRecord record) {
         // create mock market
         MarketAPI market = Global.getFactory().createMarket("fake", "fake", 5);
